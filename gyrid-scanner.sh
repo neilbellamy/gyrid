06ae2b6f05d667e63baa133e7298eac2798ede1a
neil <neil.bellamy@williamhill.com>
2014-12-17 01:30:22 +0100

    ditched json, amended log location

--- gyrid-scanner.sh
+++ gyrid-scanner.sh
@@ -4,9 +4,11 @@ clear
 
 # configure log path
 
-LOGPATH="logs"
+LOGPATH="/var/log/gyrid"
 
 # set the log files
+
+# files are named using the date at run time
 
 DETECTIONLOG="${LOGPATH}/`date +%Y%m%d`.detection.log"
 DEVICELOG="${LOGPATH}/`date +%Y%m%d`.device.log"
@@ -33,7 +35,6 @@ fi
 if [ ! -e ${DETECTIONLOG} ]
 then
   touch ${DETECTIONLOG}
-  echo -e "[\n]" >> ${DETECTIONLOG}
   echo "${START} Detection log ${DETECTIONLOG} created" >> ${SCRIPTLOG}
 else
   echo "${START} Detection log ${DETECTIONLOG} exists" >> ${SCRIPTLOG}
@@ -42,7 +43,6 @@ fi
 if [ ! -e ${DEVICELOG} ]
 then
   touch ${DEVICELOG}
-  echo -e "[\n]" >> ${DEVICELOG}
   echo "${START} Device log ${DEVICELOG} created" >> ${SCRIPTLOG}
 else
   echo "${START} Device log ${DEVICELOG} exists" >> ${SCRIPTLOG}
@@ -69,26 +69,20 @@ do
   # find out how many detections have been made so far
 
   DETECTIONS=`wc -l ${DETECTIONLOG} | awk '{print $1}'`
-  DETECTIONS=DETECTIONS-2 # do not count opening and closing braces
 
   DEVICES=`wc -l ${DEVICELOG} | awk '{print $1}'`
-  DEVICES=DEVICES-2 # do not count opening and closing braces
 
   # remember when the scan started
 
   TIME=`date +%H:%M:%S`
 
-  echo "${TIME} Scan started" >> ${SCRIPTLOG}
-  echo "${TIME} ${DEVICES} devices in ${DETECTIONS} detections" >> ${SCRIPTLOG}
+#  echo "${TIME} Scan started" >> ${SCRIPTLOG}
+#  echo "${TIME} ${DEVICES} devices in ${DETECTIONS} detections" >> ${SCRIPTLOG}
 
   # update the screen
 
   clear
   echo "                                                                BLUETOOTH SCANNER"
-  echo "================================================================================="
-  echo -e "Time\t\tMAC\t\t\tDevice name"
-  echo "---------------------------------------------------------------------------------"
-  cat ${DEVICELOG}
   echo "================================================================================="
   echo "${DETECTIONS} detections recorded in ${DETECTIONLOG}"
   echo "${DEVICES} devices recorded in ${DEVICELOG}"
@@ -115,21 +109,21 @@ do
 
       echo "${TIME} Device ${ADDR} detected with name ${NAME}" >> ${SCRIPTLOG}
 
-      # update the detections log by inserting new data before the closing bracket
+      # update the detections log
 
-      sed -i "s/\]$/  {\"time\":\"${TIME}\",\"address\":\"${ADDR}\"},\n\]/" ${DETECTIONLOG}
-      sed -i "-2s/},$/}/" ${DETECTIONLOG}
+      echo "${TIME},${ADDR}" >> ${DETECTIONLOG}
 
-      # update the devices log by inserting new data before the closing bracket
+      # update the devices log if this is a new device
 
       if ! grep -q "${ADDR}" "${DEVICELOG}"
       then 
-        sed -i "s/\]$/  {\"address\":\"${TIME}\",\"name\":\"${ADDR}\"},\n\]/" ${DEVICELOG}
-        sed -i "-2s/},$/}/" ${DEVICELOG}
+
+        echo "${ADDR},${NAME}" >> ${DEVICELOG}
+      
       fi
 
-    fi
+    fi # end of hcitool output loop
 
-  done
+  done # end of scan
 
 done
